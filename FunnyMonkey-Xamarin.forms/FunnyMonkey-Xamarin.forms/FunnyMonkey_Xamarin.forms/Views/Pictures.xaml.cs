8d4ae40c661dbebd3ecca25959a7e5bfefaadebf
Juliano <juliano.custodio@hotmail.com.br>
2017-06-13 12:26:50 -0300

    Adiciona indicadores no CarouselView

--- FunnyMonkey-Xamarin.forms/FunnyMonkey-Xamarin.forms/FunnyMonkey_Xamarin.forms/Views/Pictures.xaml.cs
+++ FunnyMonkey-Xamarin.forms/FunnyMonkey-Xamarin.forms/FunnyMonkey_Xamarin.forms/Views/Pictures.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using FunnyMonkey_Xamarin.forms.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,30 +13,22 @@ using Xamarin.Forms.Xaml;
 namespace FunnyMonkey_Xamarin.forms.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Pictures : ContentPage
+    public partial class Pictures : ContentPage, INotifyPropertyChanged
     {
         public Pictures()
         {
             InitializeComponent();
 
-            var list = new List<Monkey>();
-            var monkey1 = new Monkey();
-            var monkey2 = new Monkey();
-            var monkey3 = new Monkey();
+            
 
-            monkey1.Name = "Buzzu";
-            monkey1.Image = "https://upload.wikimedia.org/wikipedia/commons/8/85/Tamarin_portrait_2_edit3.jpg";
-            monkey2.Name = "Alfredo";
-            monkey2.Image = "https://yt3.ggpht.com/-_IXzYFNWU8U/AAAAAAAAAAI/AAAAAAAAAAA/6tXWVmD0E64/s900-c-k-no-mo-rj-c0xffffff/photo.jpg";
-            monkey3.Name = "Crush";
-            monkey3.Image = "https://i.ytimg.com/vi/PObLY87wyIU/maxresdefault.jpg";
-
-            list.Add(monkey1);
-            list.Add(monkey2);
-            list.Add(monkey3);
-
-            Carousel.ItemsSource = list;
+            Lista = new List<Monkey>() {new Monkey() {Name = "Buzzu", Image = "https://upload.wikimedia.org/wikipedia/commons/8/85/Tamarin_portrait_2_edit3.jpg"},
+                                       new Monkey() {Name = "Alfredo", Image = "https://yt3.ggpht.com/-_IXzYFNWU8U/AAAAAAAAAAI/AAAAAAAAAAA/6tXWVmD0E64/s900-c-k-no-mo-rj-c0xffffff/photo.jpg"},
+                                       new Monkey() {Name = "Crush", Image = "https://i.ytimg.com/vi/PObLY87wyIU/maxresdefault.jpg"}};
+            BindingContext = this;
         }
+        public List<Monkey> Lista { get; set; }
+        private int _position;
+        public int Position { get { return _position; } set { _position = value; OnPropertyChanged(); } }
 
     }
 }
